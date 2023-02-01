d202c90b5cf98abe05c95b8d3bc9692280a905b3
Dino <96268150+18Jack@users.noreply.github.com>
2023-02-01 17:51:42 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,26 @@
+
+# print("Hola Mundo")
+print("Las Variables en Python\n")
+
+# Suma de Enteros
+a= 2
+b= 3
+print("Suma de enteros")
+print(f"La suma es = {a + b} ")
+
+# Suma de Flotantes
+c= 2.5
+d= 3.5
+print("\nSuma de flotantes")
+print(f"La Suma de c + d es: {c +d}")
+
+c= 1.5
+print("El Nuevo valor de c es: ",c)
+
+# Solictar al usuario introducir datos 
+# Tipo string
+nombre= input("\nIntroduce tu nombre:")
+print(f"Hola {nombre}!!")
+# tipo entero
+edad= int(input("\nIntroduce tu edad:"))
+print(f"tu tienes {edad}!!")
